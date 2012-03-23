19c27ca9ffae6392186766db4e667fbc6e007131
Thomas DeMeo <thomasdemeo@gmail.com>
2012-03-23 17:46:03 -0400

    Initial Commit

---
+++ sismod.students.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name               RIT SIS Modifications
+// @description        Adds enhancements to the RIT SIS system such as
+//                     a color theme
+// @namespace          demeo.rit.sismod
+// @author             Thomas DeMeo
+// @include            http://mycampus.rit.edu/*
+// @include            https://mycampus.rit.edu/*
+// ==/UserScript==
+
+
+    function() {
+	var list = document.getElementsByClassName("")
+	
+    }
+    
+    function() {
+	var alphaTable = document.getElementById("ACE_$ICField27");
+	alphaTable.style.display = 'none';
+    }
