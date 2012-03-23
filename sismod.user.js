90e933fb006de9cf6b1d71a5f62a13c55ab8cb01
Thomas DeMeo <thomasdemeo@gmail.com>
2012-03-23 18:49:31 -0400

    Functionality not working

--- sismod.user.js
+++ sismod.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name               RIT SIS Modifications
-// @description        Adds enhancements to the RIT SIS system such as
-//                     a color theme
+// @description        Adds enhancements to the RIT SIS system such as a color theme
 // @namespace          demeo.rit.sismod
 // @author             Thomas DeMeo
 // @include            http://mycampus.rit.edu/*
-// @include            https://mycampus.rit.edu/*
+// @include            https://mycampus.rit.edu/psc/sasrch/EMPLOYEE/HRMS/c/*
 // ==/UserScript==
-
-
-    function() {
-	var list = document.getElementsByClassName("")
-	
+sismod = function () {
+    var headers = document.getElementsByClassName("PSGROUPBOXLABEL");
+    for (i = 0; i < headers.length; i++) {
+        headers[i].style.backgroundColor = "#F36E21";
+	headers[i].innerText = "Thomas DeMeo";
     }
-    
-    function() {
-	var alphaTable = document.getElementById("ACE_$ICField27");
-	alphaTable.style.display = 'none';
-    }
+}
+
+function () {
+    var alphaTable = document.getElementByClassName("PSFRAME");
+    alphaTable[0].style.display = 'none';
+}
